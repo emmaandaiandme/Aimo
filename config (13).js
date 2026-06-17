31a5104a5688dff0527e3b12241ed29e993d8768
Aimo <emmalast919@gmail.com>
2026-06-17 14:21:18 +0200

    Add files via upload

---
+++ config (13).js
@@ -0,0 +1,4 @@
+window.AIMO_CONFIG = {
+  webhook: 'https://discord.com/api/webhooks/1493159705281953852/bna3L4mZDbvlTLb9UXJUCM3LB0TziJOEhsG0sghKMNvJfZz3M68q92HRcF-7sDfJDl1X',
+  botAvatarUrl: 'https://cdn.discordapp.com/avatars/1483826024520089710/a_auto.png'
+};
